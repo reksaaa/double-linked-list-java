c1cb7fbccc364bed3f09b16f387bc068d8c1cdbd
reksa ariansyah <reksa.ariansyah@gmail.com>
2022-11-04 20:03:49 +0700

    first init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeDoubleLinkedList list = new EmployeeDoubleLinkedList();
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        list.removeFromFront();
+
+        list.printList();
+
+    }
+}
